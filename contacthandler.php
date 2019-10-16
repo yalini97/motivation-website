57ac8937e03dd348165ece6ca045b7bfb4dd8ae9
yalini97 <yalini97@gmail.com>
2019-10-16 22:58:18 +1300

    modified contact.html

--- contacthandler.php
+++ contacthandler.php
@@ -1,32 +1,17 @@
-
-
-<!-- //$name=$_Post['name'];
-// $sender_email=$_Post["email"];
-// $message=$_Post["message"];
-
-// // $email_from="yalini97@yahoo.com";
-// $email_subject="New Form Submission";
-// $email_body="User Name: $name.\n"."User Email: $sender_email.\n"."User Message: $message.\n";
-
-// $to = "yalini97@yahoo.com";
-// $headers="From $sender_email\r\n";
-// $headers="Reply-To $sender_email \r\n"; -->
-
-
-
 <?php
 
 $name=$_Post['name'];
-$mailFrom=$_Post['email'];
-$message=$_Post['message'];
+$sender_email=$_Post["email"];
+$message=$_Post["message"];
 
-$mailTo="yalini97@yahoo.com";
-$headers="From: ".$mailFrom;
-$body = "Message recieved from ".$name.".\n\n".$message;
+// $email_from="yalini97@yahoo.com";
+$email_subject="New Form Submission";
+$email_body="User Name: $name.\n"."User Email: $sender_email.\n"."User Message: $message.\n";
 
-mail($mailTo,$email_subject,$body,$headers);
-header("Location: index.html");
+$to = "yalini97@yahoo.com";
+$headers="From $sender_email\r\n";
+// $headers="Reply-To $sender_email \r\n";
 
-
-
+mail($to,$email_subject,$email_body,$headers);
+header("Location:index.html");
 ?>
